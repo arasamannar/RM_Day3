1fcd0ade251fc6b21e9b06e560f955b78e082ec1
arasamannar <arasamannar@gmail.com>
2023-10-05 23:25:44 +0530

    added updated verions on object comapre and edited the variable with let instead of var as suggested.

--- Flags.js
+++ Flags.js
@@ -3,7 +3,7 @@ request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
 request.onload=function() {
     var result=JSON.parse(request.response);
-    for (var t in result)
+    for (let t in result)
     {
         console.log(result[t].name.common, result[t].flags); 
         //gets the data from server through API and results the country name and its flag details using for-in loop.
